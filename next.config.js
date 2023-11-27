1e062009130a50c7a701bafffb773d39bad673d0
ReKnovi <sayonaradongol@gmail.com>
2023-11-27 20:17:16 +0545

    typo error fixed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    domains: ["lh3.goggleusercontent.com"],
+    domains: ["lh3.googleusercontent.com"],
   },
   webpack(config) {
     config.experiments = {
